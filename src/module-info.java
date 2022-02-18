477ce8785a7b568cee7b78faaa8fe511378246d3
Zlata Migas <zlatamigas@gmail.com>
2022-02-18 22:56:28 +0300

    Init commit

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module lab1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+
+    opens sample;
+    opens sample.controller;
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,31 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ColorMenu.fxml"));
+        Parent root = (Parent) loader.load();
+
+        primaryStage.setTitle("Color converter");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("icon.png")));
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+}
